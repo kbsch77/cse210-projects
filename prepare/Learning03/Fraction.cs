f2787097a20f4ff3114ccdd3a34f490ddafdebbb
kbsch77 <sch21099@byui.edu>
2023-02-06 11:58:33 -0700

    W06 Prepare

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,41 @@
+public class Fraction{
+    private int _top;
+    private int _bottom;
+
+    public Fraction(){
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int wholeNumber){
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom){
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public void GetTop(){
+        Console.WriteLine(_top);
+    }
+    public void SetTop(int top){
+        _top = top;
+    }
+    public void GetBottom(){
+        Console.WriteLine(_bottom);
+    }
+    public void SetBottom(int bottom){
+        _bottom = bottom;
+    }
+
+    public string GetFractionString(){
+        string fractionString = ($"{_top}/{_bottom}");
+        return fractionString;
+    }
+    public double GetDecimalValue(){
+        double top = _top;
+        double bottom = _bottom;
+        double decimalValue = top / bottom;
+        return decimalValue;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(5);
+        Fraction fraction3 = new Fraction(3, 4);
+        Fraction fraction4 = new Fraction();
+        
+
+        string fraction1String = fraction1.GetFractionString();
+        double fraction1Double = fraction1.GetDecimalValue();
+
+        Console.WriteLine(fraction1String);
+        Console.WriteLine(fraction1Double);
+
+        string fraction2String = fraction2.GetFractionString();
+        double fraction2Double = fraction2.GetDecimalValue();
+
+        Console.WriteLine(fraction2String);
+        Console.WriteLine(fraction2Double);
+
+        string fraction3String = fraction3.GetFractionString();
+        double fraction3Double = fraction3.GetDecimalValue();
+
+        Console.WriteLine(fraction3String);
+        Console.WriteLine(fraction3Double);
+
+        fraction4.SetTop(1);
+        fraction4.SetBottom(3);
+
+        Console.Write("Top: ");
+        fraction4.GetTop();
+        Console.Write("Bottom: ");
+        fraction4.GetBottom();
+
+        string fraction4String = fraction4.GetFractionString();
+        double fraction4Double = fraction4.GetDecimalValue();
+
+        
+        Console.WriteLine(fraction4String);
+        Console.WriteLine(fraction4Double);
     }
 }
